4e6bf4b1f16ae70b04a70ba28e220a56ed14ae82
PedroAugustoPereira <pereiraaugustopedro2021@gmail.com>
2024-02-29 09:57:28 -0300

    mudando teste de banco de dados

--- src/utils/connecDb.ts
+++ src/utils/connecDb.ts
@@ -1,7 +1,6 @@
-import config from "config";
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const dbUrl = config.get<string>("mongoUrl");
+const dbUrl = process.env.MONGO_URL;
 
 const connectDB = async () => {
   try {
